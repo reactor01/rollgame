d0b9f2795f16259e40bcbd09c8ce0d5ae46c1ab5
reactor01 <kmnparts1@gmail.com>
2022-06-29 13:43:23 +0300

    Fix bugs

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
+      winNumber: "",
       playedNumbers: [],
     };
   },
   methods: {
     generateNumber() {
       const generatedNumber = Math.floor(1000 + Math.random() * 9000);
-      this.playedNumbers.push(generatedNumber);
+      this.playedNumbers.unshift(generatedNumber);
+      this.winNumber = generatedNumber;
     },
   },
 });
